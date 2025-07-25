ca1ddfce621a0ceed783c1fe83a1b1e680baca72
Shubham0217 <shubham0127singh@gmail.com>
2025-07-25 23:34:26 +0530

    Added Header assertion

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,6 +1,11 @@
 package tests;
 import base.BaseTest;
 import pages.LoginPage;
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -21,6 +26,12 @@ public class LoginTest extends BaseTest {
         login.enterUsername("Admin");
         login.enterPassword("admin123");
         login.clickLogin();
+        
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement dashboardHeader = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h6[text()='Dashboard']")));
+
+        Assert.assertTrue(dashboardHeader.isDisplayed(), "Dashboard is not displayed after login.");
+        System.out.println("Header natched");
         
     }
 
